d053c7eeb4c1e9b0e09499954588c8fe95cf2f83
Rob <rdaly1490@gmail.com>
2015-06-03 21:09:50 -0500

    added hover to navbar

---
+++ dist/scripts/main.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+
+// 	$(".nav-elements").on("mouseenter", function(e) {
+// 		$(e.target).css("backgroundColor", "#rgba(0, 0, 0, 0.7)");
+// 	})
+
+// 	$(".nav-elements").on("mouseout", function(e) {
+// 		$(e.target).css("backgroundColor", "rgba(0, 0, 0, 0.5)");
+// 	})
+
+// 	$(".nav-elements a").on("mouseenter", function(e) {
+// 		$(e.target).css("backgroundColor", "#rgba(0, 0, 0, 0.7)");
+// 	})
+
+// 	$(".nav-elements a").on("mouseout", function(e) {
+// 		$(e.target).css("backgroundColor", "rgba(0, 0, 0, 0.5)");
+// 	})
+
+
+
+// });
